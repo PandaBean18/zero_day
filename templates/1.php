142c257732ddc51a72e1e5198a5e7c0415f3c91c
RaghapBean <69561650+PandaBean18@users.noreply.github.com>
2022-10-08 01:49:09 +0530

    Add files via upload

---
+++ templates/1.php
@@ -0,0 +1,83 @@
+<html>
+<title>Blog | Compass</title>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kulim+Park&family=Montserrat:wght@600&display=swap" rel="stylesheet">
+    <style>
+       h1{
+        font-family: "Montserrat",sans-serif;
+        font-size: 65px;
+        text-decoration: underline;
+        color:black;
+        margin-top: 50px;
+        margin-left: 10px;
+       }
+       p{
+
+  font-size: 25px;
+  margin-left: 10px;
+  margin-top: -25px;
+  font-style: italic;
+}
+#dea{
+    margin-left: 200px;
+    transform: translateY(-40%);
+}
+#this{
+    transform: translateX(-50%);
+    opacity: 0.7;
+}
+text{
+
+  font-size: 18px;
+
+transform: translateY(-20%);
+  font-family: "Montserrat",sans-serif;
+}
+    </style>
+    <body>
+    <h1>Public Transport in Delhi</h1>
+    <p>Delhi has had the tag of one of, if not the most, polluted cities in the country. In order to change that...</p>
+    <text>WRITTEN BY RAGHAV</text>
+    <br>
+    <svg id = "this" width="1053" height="253" viewBox="0 0 1053 253" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="-46" width="1099" height="253" rx="50" fill="#2B5490"/>
+</svg>
+
+<img id = "dea" src = "IMG/deal.png">
+<p>Delhi has had the tag of one of, if not the most, polluted cities in the country. In order to change that, the public transport in Delhi is going through a
+major overhaul. The city has to cater to millions of vehicles on a daily basis resulting in huge traffic jams, staggeringly high levels of pollution and waste of 
+quality time for its citizens. However, the mega city cannot just keep building more and more roads to cater to the hastily increasing vehicular population.
+<br><br>
+Instead, what it desperately needs is an efficient transit system along with a planned, comfortable and dependable public transport system. The public transport
+system should be able to not only reduce traffic congestion and air pollution, but also provide mobility. Good quality and services must be provided to attract
+large number of people into using it regularly.
+<br><br>
+Delhi's first step in this direction was the Delhi Metro, which began operating in 1998. The BRT system plays a very important role in a successful public
+transport system. Bus systems still have their own significance. Delhi Metro cannot completely replace the bus system on all lines. High capital cost, low
+return on investment, and long maturity period make it impossible to build subways on all sections.
+<br><br>
+BRT is a low-cost, flexible mass transit system that is much cheaper than a subway system and can serve up to 100 times the area of ​​a rail system. The BRT
+system has been shown to cost Rs for a distance of 18 km. 5-20 kroner/km compared to the metro which costs around rupees. At 125-220 kroner/km, the design 
+and construction of an 18 km line takes 1.5 years for the BRT and 5 years for the metro.
+<br><br>
+To improve public transport in the city and encourage people to use it, the Delhi Government introduced BRTS in Delhi in 2004 as part of its Integrated Mass
+Transit Plan.
+<br><br>
+The BRT proposal for Delhi was first submitted in 1996. An international workshop was held in 2001 by the Delhi Transport Corporation (DTC) and the Infrastructure 
+Development Finance Company (IDFC) where several international experts discussed ideas in the context of Delhi.
+<br><br>
+Following this, in 2002, the Delhi Government established the Sustainable Transport Commission, chaired by the Chief Commissioner, and recommended the deployment 
+of BRTS on all major corridors of Delhi. Extensive consultations were held along the proposed corridor with all stakeholders, including all relevant 
+management and construction departments, utility owners and RWA. government. also organized an international workshop in Dec 2005 where experts from all over 
+the globe and India were invited to examine the project and address the issues and doubts thrown up by various quarters. The experts observed that road-based 
+public transport was among the most effective connectivity solution for a majority of city residents. It did not matter how many vehicle lanes were there. 
+What mattered was the effective width available for vehicles. Govt. of Delhi also saw that many intelligent cities had adopted this system for the same 
+reasons of efficiency, equity, and environmental sustainability and it approved the project. BRT corridor was not merely road re-engineering but a complete 
+overhaul of the road system, wherein the most vulnerable vehicles are allocated road space, and it enabled the same right of way to move more people.
+<br><br>
+The concept, including exhaustive technical specifications and geometric design, was made by TRIPP (Transportation Research and Injury Prevention Programme), 
+IIT Delhi. ITDP (Institute for Transportation and Development Policy), New York, was associated with TRIPP in the initial stages</p>
+    </body>
+</html>
